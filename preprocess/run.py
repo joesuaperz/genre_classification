42cabe72b881378e5d4aba3ce630f9f0646fd807
Jose <joesuaperz@gmail.com>
2024-09-19 10:52:06 -0600

    Update run.py

--- preprocess/run.py
+++ preprocess/run.py
@@ -27,8 +27,8 @@ def go(args):
 
     # A minimal feature engineering step: a new feature
     logger.info("Feature engineering")
-    df['title'].fillna(value='')
-    df['song_name'].fillna(value='')
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
     df['text_feature'] = df['title'] + ' ' + df['song_name']
 
     filename = "processed_data.csv"
